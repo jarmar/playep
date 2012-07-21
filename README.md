b4307a2a8c9dcb52c587a7a49ae467f4c91d93fe
Jakob Jarmar <jakob@jarmar.se>
2012-07-21 21:45:30 +0200

    add optional search terms and optionally searching recursively when no match is found

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ player. The default is `vlc`.
 
 ## Usage
 
-`playep.py episode`, where `episode` represents an episode. Here's some 
-examples that all open episode five:
+`playep.py [term1 term2 ..]episode`, where `episode` represents an episode.
+Here's some examples that all open episode five:
 
     playep.py 5
     playep.py E05
@@ -25,13 +25,22 @@ episode (i.e. with more than one season in the same folder). Then you can write:
 
     playep.py s2e4
 
+You can also supply additional search terms that will all be required to be
+present in the filename. Examples:
+
+    playep.py br ba s2e1
+    playep.py suits 5
+
+playep will first search the current directory, and if no matches are found,
+walk through all subdirectories if the user so wants.
+
 ## Todo
+
+The recursive search walks all subdirectories to the bottom. It would be better
+to recurse in a more restrictive manner. The easiest way to do this would be
+to stop at the first file that matches the arguments, but this would lead to
+ambiguous arguments executing the first matching file, rather than the unique
+matching file.
 
 Make the 's' optional when specifying a season (allowing e.g. "2e4") (yes, 
 we're being *really* lazy here)
-
-Search subdirectories?
-
-Optionally take another argument: `playep [series] episode` where the optional 
-argument is tested (case-insensitive) against the candidate filenames.
-E.g. `playep suits 5`.
--- playep.py
+++ playep.py
@@ -14,7 +14,7 @@ def cwd_filenames():
   cwd = os.getcwd()
   return os.listdir(cwd)
 
-def make_sense_of_input(input_str):
+def normalize_episode(input_str):
   regex = '((?<=S)[0-9]+)?E?([0-9]+)'
   input_str = input_str.upper()
   m = re.search(regex, input_str)
@@ -37,35 +37,106 @@ def is_this_a_movie_file(pathname):
   extension = pathname[i + 1:]
   return extension in known_movie_types
 
-def find_that_file(ep_str):
-  filenames = cwd_filenames()
+def episode_matches(ep_str, directory, recursive=False):
+  if not recursive:
+    filenames = os.listdir(directory)
+  else:
+    filenames = []
+    for root, dirs, files in os.walk(directory):
+      for filename in files:
+        filenames.append('%s/%s' % (root, filename))
   candidates = filter(is_this_a_movie_file,
                       filter(lambda n: n.upper().find(ep_str) != -1, filenames))
+  return candidates
+
+def find_by_episode(ep_str):
+  search_subs = False
+  cwd = os.getcwd()
+  candidates = episode_matches(ep_str, cwd)
   if not candidates:
-    print "error: can't find episode " + ep_str + " in this folder."
-    exit()
-  if len(candidates) > 1:
-    print "error: it seems " + ep_str + " is not conclusive, found " +\
-          "several matching movie files:"
-    for filename in candidates:
-      print filename
-    exit()
-  return candidates[0]
+    print "can't find episode " + ep_str + " in this folder."
+    search_subs = confirm("search subdirectories?", True)
+  if search_subs:
+    candidates = episode_matches(ep_str, cwd, recursive=True)
+  return candidates
+
+def find_by_both(search_strs, ep_str):
+  search_subs = False
+  cwd = os.getcwd()
+  episode_candidates = episode_matches(ep_str, cwd)
+  candidates = filter(lambda c: all([c.lower().find(term) != -1 for term in search_strs]), episode_candidates)
+  if not candidates:
+    print "can't find %s %s in this folder." % (' '.join(search_strs), ep_str)
+    search_subs = confirm("search subdirectories?", True)
+  if search_subs:
+    episode_candidates = episode_matches(ep_str, cwd, recursive=True)
+    candidates = filter(lambda c: all([c.lower().find(term) != -1 for term in search_strs]), episode_candidates)
+  return candidates
+
+def confirm(prompt, resp=False):
+  if resp:
+    prompt = '%s %s/%s: ' % (prompt, 'Y', 'n')
+  else:
+    prompt = '%s %s/%s: ' % (prompt, 'y', 'N')
+  while True:
+    ans = raw_input(prompt)
+    if not ans:
+      return resp
+    if ans.upper() == 'Y':
+      return True
+    if ans.upper() == 'N':
+      return False
+
+def main(args):
+  if len(args) == 2:
+    episode_input = args[1]
+    episode_str = normalize_episode(episode_input)
+    movie_files = find_by_episode(episode_str)
+    if not movie_files:
+      print "unable to find episode %s!" % episode_str
+      exit()
+    if len(movie_files) > 1:
+      print "found several matching movie files:"
+      for filename in movie_files:
+        print filename
+      print "maybe supply a series name or a season?"
+      exit()
+  else:
+    search_terms = args[1:-1]
+    episode_input = args[-1]
+    search_strs = [term.lower() for term in search_terms]
+    episode_str = normalize_episode(episode_input)
+    movie_files = find_by_both(search_strs, episode_str)
+    if not movie_files:
+      print "unable to find %s %s!" % (' '.join(search_strs), episode_str)
+      exit()
+    if len(movie_files) > 1:
+      print "found several matching movie files:"
+      for filename in movie_files:
+        print filename
+      print "try to narrow down your arguments."
+      exit()
+
+  movie_file = movie_files[0]
+  print "found file %s. opening with %s..." % (movie_file, movie_player_of_choice)
+  os.execlp(movie_player_of_choice, movie_player_of_choice, movie_file)
 
 if __name__ == '__main__':
-  try:
-    input_str = sys.argv[1]
-  except:
-    print "Usage: 'playep.py <episode>' where <episode> has a format similar"
-    print "to the following (all these will open episode 5):"
-    print "'5', 'S01E05', 'e5', 's1e5'"
-    print "Specifying the season is optional, and if omitted is disregarded"
-    print "when searching for the right file."
-    print "The movie player to use is selected by changing the"
-    print "'movie_player_of_choice' variable in playep."
-    exit()
-  ep_str   = make_sense_of_input(input_str)
-  filename = find_that_file(ep_str)
-  print "found file '" + filename + "'. opening with '" +\
-      movie_player_of_choice + "'..."
-  os.execlp(movie_player_of_choice, movie_player_of_choice, filename)
+  if len(sys.argv) < 2:
+    print """Usage: 'playep.py [term1 term2 ..] episode' where episode has a"
+format similar to the following (all these will open episode 5):
+  '5', 'S01E05', 'e5', 's1e5'
+Specifying the season is optional, and if omitted is disregarded
+when searching for the right file.
+Optionally, additional search terms can be added to distinguish
+the sought file (typically, the name of the series).
+All search terms must be present in the file name.
+Example:
+  'playep.py br ba s2e1', 'playep.py suits 5'
+
+The script initially searches the current directory, and if no
+match is found, asks if the user wants to search all subdirectories as well.
+The movie player to use is selected by changing the
+'movie_player_of_choice' variable in playep."""
+  else:
+    main(sys.argv)
